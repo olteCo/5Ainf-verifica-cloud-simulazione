31611383fa10397a4205c2f5e61f78b51e1e7a3a
Your Name <you@example.com>
2019-04-10 13:21:12 +0200

    parte dinamica

--- code/js/app.js
+++ code/js/app.js
@@ -7,7 +7,21 @@ var app = {
 	},
 	showCards(dataJson) {
 		console.log(dataJson);
-		
+		dataJson.cardList.forEach(element => {
+			let html= `<div class="col-lg-3 col-md-6 mb-4 net-all">
+			<div class="card h-100">
+			  <img class="card-img-top" src="${element.imgURL}">
+			  <div class="card-body">
+				<h4 class="card-title">${element.titolo}</h4>
+				<p class="card-text">${element.desc}</p>
+			  </div>
+			  <div class="card-footer">
+				<a href="${element.buttonURL}" class="btn btn-primary">Accedi</a>
+			  </div>
+			</div>
+		  </div>`;
+		  $("#card-container").append(html);
+		});
 	},
 	onError: function(e) {
 		console.log(`Ajax error: ${JSON.stringify(e)}`);
